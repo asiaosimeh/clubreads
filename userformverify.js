e078e5c787f63121cbcabde2360464ceb26abc6c
Asia Osimeh <osimeha1@southernct.edu>
2024-11-05 00:42:04 +0000

    user registration form js file

---
+++ userformverify.js
@@ -0,0 +1,33 @@
+function userPass(){
+	let pass = document.getElementById('pass').value;
+	let repeatpass = document.getElementById('repeat').value;
+
+
+	
+	if (pass === repeatpass){
+		console.log("Passwords match!");
+	} else {
+		console.log("Passwords do not match, please enter same password");
+		}
+	
+	
+	let message = document.getElementById("results").value;
+	
+}
+
+function validEmail(){
+	
+	let emailUser = document.getElementById('user').value;
+	let message = document.getElementById('results').value;
+	
+	let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	
+	if (emailRegex.test(emailUser)){
+		message.textContent = "";
+		
+	} else{
+		message.textContent = "Please enter a valid email address";
+	}
+}
+
+console.log("The verification js file");
