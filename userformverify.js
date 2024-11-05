63b7dd3b0f02f464e251cefaefb1490d53b8cf89
Asia Osimeh <osimeha1@southernct.edu>
2024-11-05 01:55:25 +0000

    changes 8:55pm

--- userformverify.js
+++ userformverify.js
@@ -1,32 +1,36 @@
 function userPass(){
+
 	let pass = document.getElementById('pass').value;
 	let repeatpass = document.getElementById('repeat').value;
-
+	let message = document.getElementById('results');
 
 	
 	if (pass === repeatpass){
+		message.textContent = "Passwords match";
 		console.log("Passwords match!");
 	} else {
+		message.textContent = "Passwords don't match";
 		console.log("Passwords do not match, please enter same password");
 		}
 	
 	
-	let message = document.getElementById("results").value;
 	
 }
 
 function validEmail(){
 	
 	let emailUser = document.getElementById('user').value;
-	let message = document.getElementById('results').value;
+	let message = document.getElementById('results');
 	
 	let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 	
 	if (emailRegex.test(emailUser)){
 		message.textContent = "";
+		console.log("Valid email");
 		
 	} else{
 		message.textContent = "Please enter a valid email address";
+		console.log("Not valid");
 	}
 }
 
