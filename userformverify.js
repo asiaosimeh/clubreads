79eabb4f85e39d5730c1139afbdd60a2dae1c77f
Asia Osimeh <osimeha1@southernct.edu>
2024-11-12 15:29:44 +0000

    Resolved conflict and commited changes

--- userformverify.js
+++ userformverify.js
@@ -10,10 +10,22 @@ function userPass(){
 		console.log("Passwords match!");
 	} else {
 		message.textContent = "Passwords don't match";
+
+
+function userPass(){
+	let pass = document.getElementById('pass').value;
+	let repeatpass = document.getElementById('repeat').value;
+
+
+	
+	if (pass === repeatpass){
+		console.log("Passwords match!");
+	} else {
 		console.log("Passwords do not match, please enter same password");
 		}
 	
 	
+	let message = document.getElementById("results").value;
 	
 }
 
@@ -21,6 +33,8 @@ function validEmail(){
 	
 	let emailUser = document.getElementById('user').value;
 	let message = document.getElementById('results');
+	
+	let message = document.getElementById('results').value;
 	
 	let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 	
@@ -31,6 +45,9 @@ function validEmail(){
 	} else{
 		message.textContent = "Please enter a valid email address";
 		console.log("Not valid");
+		
+	} else{
+		message.textContent = "Please enter a valid email address";
 	}
 }
 
