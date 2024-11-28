07be9aaaf1cdf7cedb41a44d93e66f0cf31678cc
shaham150 <shahame1@southernct.edu>
2024-11-28 23:29:16 +0000

    Added fields to registration form

--- src/js/userformverify.js
+++ src/js/userformverify.js
@@ -1,18 +1,22 @@
 
-
+// Verify format of both password fields:
 function userPass(){
 	let pass = document.getElementById('pass').value;
 	let repeatpass = document.getElementById('repeat').value;
 	
 	if (pass == "" || repeatpass == ""){
 		window.alert("Please fill out both password fields.");
+		return false;
 	} else if (pass === repeatpass){
-		window.alert("Passwords match!");
+		//window.alert("Passwords match!");
+		return true;
 	} else {
 		window.alert("Passwords do not match, please enter same password");
+		return false;
 	}	
 }
 
+// Verify format of email field:
 function validEmail(){
 	
 	let emailUser = document.getElementById('email').value;
@@ -21,12 +25,24 @@ function validEmail(){
 	
 	if (!emailRegex.test(emailUser)){
 		window.alert("Please enter a valid email address");
+		return false;
+	} else {
+		return true;
 	}
 }
 
 document.getElementById("submitBtn").addEventListener("click", function (){
-	validEmail();
-	userPass();
+	let username = document.getElementById("username").value;
+	let email = document.getElementById("email").value;
+	let fname = document.getElementById("fname").value;
+	let lname = document.getElementById("lname").value;
+	let pass = document.getElementById("pass").value; // Only taking 'pass' and not 'repeat' because info will only one needs to be sent to server if they match
+	
+	if (username=="" || fname=="" || lname=="") {
+		window.alert("Please fill out all fields");
+	} else if (validEmail() && userPass() && username!="" && fname!="" && lname!=""){
+		console.log("INPUT: ", username, email, fname, lname, pass);
+	}
 });
 
 console.log("The verification js file");
