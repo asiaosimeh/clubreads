5639c63a6e8f10c4702be0db66b768e1e08e5cde
Asia Osimeh <osimeha1@southernct.edu>
2024-12-01 21:07:53 +0000

    Added changes for admin db

--- server.js
+++ server.js
@@ -166,6 +166,29 @@ function rtrvDB(topic, author, loc) {
 	return response;
 }
 
+//Display all submitted tickets on the ClubReads Admin's dashboard
+function adminIT(res){
+	
+	let queryStr = "SELECT * FROM it_ticket_form;";
+
+	
+	let connection_pool = mysql.createPool(connectionObj_IT);
+	connection_pool.query(queryStr, function (error, results){
+		if (error) {
+			console.log("ERROR: ", error);
+			res.writeHead(500, {'Content-Type': 'application/json'});
+			res.end(JSON.stringify({error: "Database error occurred."}));
+		}
+		else{
+			console.log("Connection established");
+			console.log(results);
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.write(JSON.stringify(results));
+			res.end();
+		}
+	
+	});
+}
 
 
 // Main function, decides which other function to call to server the client's request:
