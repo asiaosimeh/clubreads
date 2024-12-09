d69715a8e3694c5bd2a2441783fbe65c18ad5c78
shaham150 <shahame1@southernct.edu>
2024-12-09 18:06:53 +0000

    Full functionality for editing an existing book club

--- src/js/mbdash.js
+++ src/js/mbdash.js
@@ -33,16 +33,31 @@ function pasteData(data){
 	let pasteBin = document.getElementById("clubsList").innerHTML;
 	console.log(pasteBin, pasteBin.innerHTML);
 	document.getElementById("clubsList").innerHTML = "";
+	
+	if (data == null || data == ""){
+		document.getElementById("clubsList").innerHTML = "<h2>You are not currently hosting any clubs.</h2>";
+	}
+	
+	// Initializing a list of club IDs hosted, for later use (editBC.js):
+	let idList = ""; // Making this a string, it iwll be split into elements later
 
 	for (element of data){
 		console.log(element);
-		document.getElementById("clubsList").innerHTML += "<br><details style='font-size:1.2em'><summary style='font-size:1.2em'>" + element[1] + "</summary><p style='font-size:1em'><strong>Book:</strong> " + element[2] + "<br><strong>Author:</strong> " + element[3] + "<br><strong>Genre:</strong> " + element[4] + "<br><strong>Meeting Day:</strong> " + element[5] +  "</p><button type='button' id='editClub" + element[0] + "' onclick='editClub()'>Edit Club</button></summary>";
-	}
+		document.getElementById("clubsList").innerHTML += "<br><details style='font-size:1.2em'><summary style='font-size:1.2em'>" + element[1] + "</summary><p style='font-size:1em'><strong>Club ID:</strong> " + element[0] + "<br><strong>Book:</strong> " + element[2] + "<br><strong>Author:</strong> " + element[3] + "<br><strong>Genre:</strong> " + element[4] + "<br><strong>Meeting Day:</strong> " + element[5] +  "</p></summary>";
+		
+		idList += element[0] + ",";
+		
+		/*let btn = document.getElementById(element[0].toString());
+		console.log("BUTTON - ", btn, btn.innerHTML);
+		btn.addEventListener("click", function(){
+			window.alert("CLICKED BUTTON - ");
+		});*/
+	} // End for loop
+
+	sessionStorage.setItem("clubIDList", idList);
+
 }
 
-function editClub(){
-	window.alert("Editing funcitonality coming soon!");
-}
 
 // Run the output command onload:
 loadInfo();
